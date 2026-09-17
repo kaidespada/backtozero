a2027dbef97cfcbc640547d4fb42bf49563e2892
Iva <domerca.ru@gmail.com>
2026-09-17 15:39:02 +0300

    Вернул все задачи с 1 по 11 в репозиторий

---
+++ C#/PR2CH4/task2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR2CH4
+{
+    internal class task2
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Введите длину массива N");
+            int N = Convert.ToInt32 (Console.ReadLine());
+            int[] array = new int[N];
+            
+            for(int i = 0; i < N; i++)
+            {
+                Console.WriteLine($"Введите элемент {i}");
+                array[i] = Convert.ToInt32 (Console.ReadLine());
+            }
+            Console.WriteLine(" ");
+            for (int i = N - 1; i >= 0; i--)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}

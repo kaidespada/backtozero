e99a85c124e2e7695453dcd9ae76173dca6098f3
Iva <domerca.ru@gmail.com>
2026-09-18 16:19:22 +0300

    Добавлено решение задачи 20

---
+++ C#/PR2CH4/task20.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR2CH4
+{
+    internal class task20
+    {
+        public static void Run()
+        {
+
+            int[] numbers = new int[10];
+            Random random = new Random();
+
+            Console.Write("Массив до: ");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                numbers[i] = Convert.ToInt16(random.Next(-5, 15));
+                Console.Write(numbers[i] + " ");
+
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Введите индекс элемента для удаления: ");
+            int indexToRemove = Convert.ToInt32(Console.ReadLine());
+
+            if (indexToRemove > 0 || indexToRemove >= numbers.Length)
+            {
+                Console.WriteLine("Ошибка: введен некорректный индекс!");
+            }
+            else
+            {
+                int[] result = new int[numbers.Length - 1];
+                int resultIndex = 0;
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (i == indexToRemove)
+                    {
+                        continue;
+                    }
+
+                    result[resultIndex] = numbers[i];
+                    resultIndex++;
+                }
+
+                numbers = result;
+            }
+
+            Console.Write("Массив после: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

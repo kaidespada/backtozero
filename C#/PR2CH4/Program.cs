0e37702a2259d83de189704ccbb7133a5524d57d
Iva <domerca.ru@gmail.com>
2026-09-18 17:45:16 +0300

    Добавлено решение задачи 22

--- C#/PR2CH4/Program.cs
+++ C#/PR2CH4/Program.cs
@@ -35,7 +35,8 @@ namespace PR2CH4
                     case 19: task19.Run(); break;
                     case 20: task20.Run(); break;
                     case 21: task21.Run(); break;
-                    //case 22: task22.Run(); break;
+                    case 22: task22.Run(); break;
+                    case 23: task23.Run(); break;
 
                     default: Console.WriteLine("Нет такой задачи"); break;
                 }
---
+++ C#/PR2CH4/task22.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR2CH4
+{
+    internal class task22
+    {
+        public static void Run()
+        {
+            int[] numbers = new int[10];
+            Random random = new Random();
+
+            Console.Write("Массив: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(-5, 15);
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+
+            int[] tempResult = new int[numbers.Length];
+            int uniqueCount = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                bool isDuplicate = false;
+
+                for (int j = 0; j < uniqueCount; j++)
+                {
+                    if (numbers[i] == tempResult[j])
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+
+                if (!isDuplicate)
+                {
+                    tempResult[uniqueCount] = numbers[i];
+                    uniqueCount++;
+                }
+            }
+
+            int[] result = new int[uniqueCount];
+            for (int i = 0; i < uniqueCount; i++)
+            {
+                result[i] = tempResult[i];
+            }
+
+            Console.Write("Результат: ");
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write(result[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

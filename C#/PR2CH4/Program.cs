a74d3e5053573e723659b16ef6b9b267bbdf726e
Iva <domerca.ru@gmail.com>
2026-09-18 18:00:33 +0300

    Добавлено решение задачи 28

--- C#/PR2CH4/Program.cs
+++ C#/PR2CH4/Program.cs
@@ -41,6 +41,8 @@ namespace PR2CH4
                     case 25: task25.Run(); break;
                     case 26: task26.Run(); break;
                     case 27: task27.Run(); break;
+                    case 28: task28.Run(); break;
+
 
                     default: Console.WriteLine("Нет такой задачи"); break;
                 }
---
+++ C#/PR2CH4/task28.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR2CH4
+{
+    internal class task28
+    {
+        public static void Run()
+        {
+            int[,] matrix = new int[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 0, 1, 2 }
+            };
+
+            Console.WriteLine("Матрица:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int maxRowIndex = 0;
+            int maxSum = int.MinValue;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int currentRowSum = 0;
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    currentRowSum += matrix[i, j];
+                }
+
+                if (currentRowSum > maxSum)
+                {
+                    maxSum = currentRowSum;
+                    maxRowIndex = i;
+                }
+            }
+
+            Console.WriteLine($"Максимальная сумма у строки {maxRowIndex}: {maxSum}");
+        }
+    }
+}

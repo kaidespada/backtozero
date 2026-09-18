fb9a5c59571a72ae916e2b5ed02b00db73e84632
Iva <domerca.ru@gmail.com>
2026-09-18 15:15:07 +0300

    Добавлено решение задачи 16

---
+++ C#/PR2CH4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PR2CH4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.Write("Введите номер задачи (1-30, 0-выход): ");
+                if (!int.TryParse(Console.ReadLine(), out int num) || num == 0) break;
+
+                Console.Clear();
+                switch (num)
+                {
+                    case 1: task1.Run(); break;
+                    case 2: task2.Run(); break;
+                    case 3: task3.Run(); break;
+                    case 4: task4.Run(); break;
+                    case 5: task5.Run(); break;
+                    case 6: task6.Run(); break;
+                    case 7: task7.Run(); break;
+                    case 8: task8.Run(); break;
+                    case 9: task9.Run(); break;
+                    case 10: task10.Run(); break;
+                    case 11: task11.Run(); break;
+                    case 12: task12.Run(); break;
+                    case 13: task13.Run(); break;
+                    case 14: task14.Run(); break;
+                    case 15: task15.Run(); break;
+                    case 16: task16.Run(); break;
+
+                    default: Console.WriteLine("Нет такой задачи"); break;
+                }
+                Console.WriteLine("\nНажмите клавишу...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ C#/PR2CH4/task16.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR2CH4
+{
+    internal class task16
+    {
+        public static void Run()
+        {
+
+            int[] numbers = new int[10];
+            Random random = new Random();
+
+            Console.Write("Массив: ");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                numbers[i] = Convert.ToInt16(random.Next(-5, 15));
+                Console.Write(numbers[i] + " ");
+
+            }
+
+            bool isSorted = true;
+
+            for (int i = 1; i <  numbers.Length; i++)
+            {
+                if (numbers[i] < numbers[i - 1])
+                {
+                    isSorted = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine();
+
+            if (isSorted)
+            {
+                Console.WriteLine("Упорядочен");
+            }
+            else
+            {
+                Console.WriteLine("Не упорядочен");
+            }
+            
+        }
+    }
+}

d366e10c329b2974fd9cdbfaa89024d7ea9eb0f1
Iva <domerca.ru@gmail.com>
2026-09-18 16:10:14 +0300

    Добавлено решение задачи 19

--- C#/PR2CH4/Program.cs
+++ C#/PR2CH4/Program.cs
@@ -32,6 +32,7 @@ namespace PR2CH4
                     case 16: task16.Run(); break;
                     case 17: task17.Run(); break;
                     case 18: task18.Run(); break;
+                    case 19: task19.Run(); break;
 
                     default: Console.WriteLine("Нет такой задачи"); break;
                 }
---
+++ C#/PR2CH4/task19.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR2CH4
+{
+    internal class task19
+    {
+        public static void Run()
+        {
+
+            int[] numbers = new int[10];
+            Random random = new Random();
+
+            Console.Write("Массив до: ");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                numbers[i] = Convert.ToInt16(random.Next(-5, 15));
+                Console.Write(numbers[i] + " ");
+
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Введите число K");
+            int K = Convert.ToInt32(Console.ReadLine());
+
+            if (K < 0)
+            {
+                K = (K % numbers.Length) + numbers.Length;
+            }
+
+            K = K % numbers.Length;
+
+            int[] result = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int newIndex = (i + K) % numbers.Length;
+                result[newIndex] = numbers[i];
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = result[i];
+            }
+
+            Console.Write("Массив после: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

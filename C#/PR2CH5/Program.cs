eaa86d3de8578bf9c97a56d7c02189474c3217fe
Iva <domerca.ru@gmail.com>
2026-09-19 12:28:42 +0300

    Добавлено решение задачи 10

--- C#/PR2CH5/Program.cs
+++ C#/PR2CH5/Program.cs
@@ -24,7 +24,7 @@ namespace PR2CH4
                     case 7: task7.Run(); break;
                     case 8: task8.Run(); break;
                     case 9: task9.Run(); break;
-                    //case 10: task10.Run(); break;
+                    case 10: task10.Run(); break;
                     //case 11: task11.Run(); break;
                     //case 12: task12.Run(); break;
                     //case 13: task13.Run(); break;
---
+++ C#/PR2CH5/task10.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Dynamic;
+
+namespace PR2CH4
+{
+    internal static class task10
+    {
+        public static void Run()
+        {
+            int[] numbers = { 7, -3, 15, 0, 42, 8, -10 };
+
+            GetMinMax(numbers, out int minResult, out int maxResult);
+
+            Console.WriteLine($"Массив: [{string.Join(", ", numbers)}]");
+            Console.WriteLine("Минмум: " + minResult);
+            Console.WriteLine("Максимум: " + maxResult);
+            
+        }
+
+        private static void GetMinMax(int[] numbers, out int max, out int min)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                min = 0;
+                max = 0;
+                return;
+            }
+
+            min = numbers[0];
+            max = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+        }
+    }
+}

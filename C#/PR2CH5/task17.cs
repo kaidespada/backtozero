3a55cec25593c88ca8a2efe8d9fa7d0a80a5d047
Iva <domerca.ru@gmail.com>
2026-09-19 13:50:02 +0300

    Добавлено решение задачи 17

---
+++ C#/PR2CH5/task17.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PR2CH4
+{
+    internal static class task17
+    {
+        public static void Run()
+        {
+            int num1 = 1234;
+            Console.WriteLine($"Сумма цифр числа {num1} = {SumDigits(num1)}");
+
+            int num2 = -508;
+            Console.WriteLine($"Сумма цифр числа {num2} = {SumDigits(num2)}");
+
+            int num3 = 7;
+            Console.WriteLine($"Сумма цифр числа {num3} = {SumDigits(num3)}");
+        }
+
+        private static int SumDigits(int number)
+        {
+            if (number < 0)
+            {
+                number = -number;
+            }
+
+            if (number < 10)
+            {
+                return number;
+            }
+
+            return (number % 10) + SumDigits(number / 10);
+        }
+    }
+}

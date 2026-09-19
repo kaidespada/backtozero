d8be3fb15c998eedfeb7f8127441fc6ae9958110
Iva <domerca.ru@gmail.com>
2026-09-19 12:40:21 +0300

    Добавлено решение задачи 12

---
+++ C#/PR2CH5/task12.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PR2CH4
+{
+    internal static class task12
+    {
+        public static void Run()
+        {
+            int resultMany = Sum(1, 2, 3, 4, 5);
+            Console.WriteLine($"Сумма (1, 2, 3, 4, 5) = {resultMany}");
+
+            int resultOne = Sum(10);
+            Console.WriteLine($"Сумма (10) = {resultOne}");
+
+            int resultEmpty = Sum();
+            Console.WriteLine($"Сумма без аргументов () = {resultEmpty}");
+        }
+
+        private static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
+}

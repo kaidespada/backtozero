a006bbe2f378549a00c63329546551b3acb2fc46
Iva <domerca.ru@gmail.com>
2026-09-19 11:21:21 +0300

    Добавлено решение задачи 5

---
+++ C#/PR2CH5/task5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PR2CH4
+{
+    internal static class task5
+    {
+        public static void Run()
+        {
+            Console.Write("Введите набранные баллы (0-100): ");
+            int userScore = Convert.ToInt32(Console.ReadLine());
+
+            string grade = GetGrade(userScore);
+
+            Console.WriteLine($"Результат: {grade}");
+        }
+
+        private static string GetGrade(int score)
+        {
+            if (score < 0 || score > 100)
+            {
+                return "Ошибка: неверное количество баллов!";
+            }
+
+            if (score >= 90)
+            {
+                return "Отлично"; 
+            }
+            else if (score >= 74)
+            {
+                return "Хорошо"; 
+            }
+            else if (score >= 60)
+            {
+                return "Удовлетворительно"; 
+            }
+            else
+            {
+                return "Неудовлетворительно"; 
+            }
+        }
+    }
+}

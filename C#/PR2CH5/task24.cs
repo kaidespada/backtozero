869c516c141cd35ebbb7618d18e295b2916b88bf
Iva <domerca.ru@gmail.com>
2026-09-19 14:45:13 +0300

    Добавлено решение задачи 24

---
+++ C#/PR2CH5/task24.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PR2CH4
+{
+    internal static class task24
+    {
+        public static void Run()
+        {
+            Console.Write("Введите температуру: ");
+            if (int.TryParse(Console.ReadLine(), out int temp))
+            {
+                string category = temp switch
+                {
+                    < -10 => "мороз",
+                    >= -10 and < 15 => "холодно",
+                    >= 15 and < 30 => "тепло",
+                    >= 30 => "жарко"
+                };
+
+                Console.WriteLine(category);
+            }
+        }
+    }
+}

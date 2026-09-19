b7e427f8422d4dc16395c9699300b1a283d4d8ca
Iva <domerca.ru@gmail.com>
2026-09-19 12:19:37 +0300

    Добавлено решение задачи 9

--- C#/PR2CH5/Program.cs
+++ C#/PR2CH5/Program.cs
@@ -23,7 +23,7 @@ namespace PR2CH4
                     case 6: task6.Run(); break;
                     case 7: task7.Run(); break;
                     case 8: task8.Run(); break;
-                    //case 9: task9.Run(); break;
+                    case 9: task9.Run(); break;
                     //case 10: task10.Run(); break;
                     //case 11: task11.Run(); break;
                     //case 12: task12.Run(); break;
---
+++ C#/PR2CH5/task9.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PR2CH4
+{
+    internal static class task9
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Введите строку для преобразования в число: ");
+            string input = Console.ReadLine();
+            
+            if (TryReadInt(input, out int resultNumber))
+            {
+                Console.WriteLine("Введено корректное число: " + resultNumber);
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Введенная строка не корректна.");
+            }
+
+        }
+
+        private static bool TryReadInt(string text, out int number)
+        {
+            return int.TryParse(text, out number);
+        }
+    }
+}

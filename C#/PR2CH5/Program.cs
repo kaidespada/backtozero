c667f6c2ba630121dfe3827223eb5e932e220e0b
Iva <domerca.ru@gmail.com>
2026-09-19 13:44:13 +0300

    Добавлено решение задачи 16

--- C#/PR2CH5/Program.cs
+++ C#/PR2CH5/Program.cs
@@ -30,7 +30,7 @@ namespace PR2CH4
                     case 13: task13.Run(); break;
                     case 14: task14.Run(); break;
                     case 15: task15.Run(); break;
-                    //case 16: task16.Run(); break;
+                    case 16: task16.Run(); break;
                     //case 17: task17.Run(); break;
                     //case 18: task18.Run(); break;
                     //case 19: task19.Run(); break;
---
+++ C#/PR2CH5/task16.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PR2CH4
+{
+    internal static class task16
+    {
+        public static void Run()
+        {
+            int num1 = 5; 
+            Console.WriteLine($"Сумма чисел от 1 до {num1} = {SumToN(num1)}");
+
+            int num2 = 1;
+            Console.WriteLine($"Сумма чисел от 1 до {num2} = {SumToN(num2)}");
+
+            try
+            {
+                int num3 = 0;
+                Console.WriteLine($"Сумма чисел от 1 до {num3} = {SumToN(num3)}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                Console.ResetColor();
+            }
+        }
+
+        private static int SumToN(int n)
+        {
+            if (n < 1)
+            {
+                throw new ArgumentException("Число n должно быть > или = 1");
+            }
+
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n + SumToN(n - 1);
+        }
+    }
+}

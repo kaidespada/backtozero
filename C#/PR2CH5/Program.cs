cf495558099757a11ffd8f85b360ac33646fd9ad
Iva <domerca.ru@gmail.com>
2026-09-19 11:33:43 +0300

    Добавлено решение задачи 6

--- C#/PR2CH5/Program.cs
+++ C#/PR2CH5/Program.cs
@@ -20,7 +20,7 @@ namespace PR2CH4
                     case 3: task3.Run(); break;
                     case 4: task4.Run(); break;
                     case 5: task5.Run(); break;
-                    //case 6: task6.Run(); break;
+                    case 6: task6.Run(); break;
                     //case 7: task7.Run(); break;
                     //case 8: task8.Run(); break;
                     //case 9: task9.Run(); break;
---
+++ C#/PR2CH5/task6.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PR2CH4
+{
+    internal static class task6
+    {
+        public static void Run()
+        {
+
+            Console.Write("Введите делимое: ");
+            double a= Convert.ToInt64(Console.ReadLine());
+
+            Console.Write("Введите делитель: ");
+            double b = Convert.ToInt64(Console.ReadLine());
+
+            if(TryDivide(a, b, out double divResult))
+            {
+                Console.WriteLine("Результат деления: " + divResult);
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: делить на ноль нельзя");
+            }
+        }
+
+        private static bool TryDivide(double a, double b, out double result)
+        {
+            if (b == 0)
+            {
+                result = 0;
+                return false;
+            }
+            else
+            {
+                result = a / b;
+                return true;
+            }
+        }
+    }
+}
